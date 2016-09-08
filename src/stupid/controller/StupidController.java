703e6526882f0a843502337967090ce6dbb7f0a4
GearShift24 <jwri6847@740s-pl-369.local>
2016-09-08 08:39:55 -0600

    Added myName Construction in a PrivateString

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,9 +1,17 @@
 package stupid.controller;
 
 public class StupidController {
-
+	
+	private String myName;
+	
+	 public StupidController()
+	 {
+		 myName = "Joseph Wrigley";
+	 }
+	 
 		public void start()
 		{
 			System.out.println("This program is less stupid");
+			System.out.println(myName);
 		}
 }
