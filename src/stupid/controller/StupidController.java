1f03d6d1b8ab853416ff84e7db935fe6fe1de1dc
GearShift24 <jwri6847@740s-pl-369.local>
2016-09-08 09:54:14 -0600

    Added private voids  to ask/answer

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,17 +1,29 @@
 package stupid.controller;
 
-public class StupidController {
-	
+import java.util.Scanner;
+
+public class StupidController 
+{
 	private String myName;
+	private Scanner inputReader;
 	
 	 public StupidController()
 	 {
 		 myName = "Joseph Wrigley";
+		 inputReader = new Scanner(System.in);
 	 }
 	 
 		public void start()
 		{
 			System.out.println("This program is less stupid");
 			System.out.println(myName);
+			askQuestion();
+		}
+		
+		private void askQuestion()
+		{
+			System.out.println("How was your day?");
+			String input = inputReader.nextLine();
+			System.out.println("Oh, you said: " + input + " about your day");
 		}
 }
