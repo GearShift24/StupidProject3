b0055aecfb50748207b7a0a4c156109cad657960
GearShift24 <jwri6847@740s-pl-369.local>
2016-09-12 08:47:56 -0600

    added a double string input

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -22,18 +22,24 @@ public class StupidController
 		
 		private void askQuestion()
 		{
-			System.out.println("How was your day?");
+			System.out.println("Ok, lets start off easy. How was your day?");
 			String input = inputReader.nextLine();
-			System.out.println("Oh, you said: " + input + " about your day");
+			System.out.println("Nice, your day was " + input + ". Mine is fine, thanks for asking :|");
 		
 		
-			System.out.println("My next question is this: What is the best type of food?");
-			String foodInput = inputReader.nextLine();
-			System.out.println("Ok,you typed this: " + foodInput);
+			System.out.println("Here goes the next question: What type of pizza is your favorite?");
+			String foodInput = inputReader.next();
+			System.out.println("Wow, you like: " + foodInput + "I HATE that flavor zzz");
 			
 			System.out.println("Ok lets continue, How many dogs do you have?");
 			int dogInput = inputReader.nextInt();
 			System.out.println("Well thats depressing, you said you had: " + dogInput);
+			
+			System.out.println("While we're doing numbers, what is your favorite decimal? It can as long as you want bby!");
+			Double favNumbInput = inputReader.nextDouble();
+			System.out.println("Hey fam, " + favNumbInput + " Is a crap number. Pick a better one next time.");
+			
+			
 			
 		}
 }
